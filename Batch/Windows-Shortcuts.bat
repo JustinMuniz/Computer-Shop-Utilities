c0aa1620a9e59770baafcddffc3cc391c0bb055f
Justin Muniz <justin.muniz@maine.edu>
2016-08-27 15:56:34 -0400

    Update Windows-Shortcuts.bat
    Began creation of user interface. Brainstormed ideas for shortcuts to be featured.

--- Batch/Windows-Shortcuts.bat
+++ Batch/Windows-Shortcuts.bat
@@ -1,4 +1,28 @@
 @ECHO OFF
 
+:menuLOOP
+	cls
+	echo.
+	echo.===  H.E.L.P.e.R. Build Script  ================================
+	echo.===         Justin Muniz        ================================
+	echo.===          2016-08-15         ================================
+	echo.
+	echo.
+	for /f "tokens=1,2,* delims=_ " %%A in ('"findstr /b /c:":menu_" "%~f0""') do (
+		echo.  %%B  %%C)
+	set choice=
+	echo.&set /p choice=. Make a choice or hit ENTER to quit: ||GOTO:EOF
+	echo.&call:menu_%choice%
+GOTO:menuLOOP
+
+:menu_1		Disable password requirement at boot
+	cls
+	echo.
+	echo. The User Accounts Control Panel is opening in another window...
+	netplwiz
+GOTO:EOF
+
 REM god mode
 REM disk management
+REM settings (windows 8, 10)
+REM
