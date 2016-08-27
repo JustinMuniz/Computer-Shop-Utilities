3546ae8b3d2abc68518e9b3d80d421c141df7135
Justin Muniz <justin.muniz@maine.edu>
2016-08-27 15:58:11 -0400

    Create Run-Maintenance.bat
    Initial iteration of a routine maintenance script. Currently in brainstorming phase.

---
+++ Batch/Run-Maintenance.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+
+REM windows update
+REM degfrag
+REM disk cleanup
+REM ccleaner scan
+REM avast scan
+REM malwarebytes scan
+REM update flash
+REM update java
+REM update acrobat reader
+REM update malwarebytes
+REM update avast
+REM update ccleaner
+REM update defrag
+REM update apple software
+REM update chrome
+REM update firefox
+REM update spybot
+REM spybot scan
