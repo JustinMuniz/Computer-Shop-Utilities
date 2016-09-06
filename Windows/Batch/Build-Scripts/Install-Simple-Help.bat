37f51ea24d29a43cb53dfa190f07cd7b61a6a67c
Justin Muniz <justin.muniz@maine.edu>
2016-09-05 21:40:13 -0400

    Update Install-Simple-Help.bat
    Instead of opening a web page to start a download, the 32-bit app is downloaded and installed to the user's desktop.
    This installation is now automated.

--- Windows/Batch/Build-Scripts/Install-Simple-Help.bat
+++ Windows/Batch/Build-Scripts/Install-Simple-Help.bat
@@ -3,7 +3,8 @@ SETLOCAL
 
 CLS
 ECHO.
-ECHO. Opening Simple Help download page in another window...
-START http://macrevival.noip.me/customer
+ECHO. Initializing Simple Help download.
+bitsadmin.exe /transfer "MacRevival Simple Help" "http://macrevival.noip.me/customer/Remote%20Support-windows32-offline.exe?language=en&hostname=http%3A%2F%2Fmacrevival.noip.me&ie=ie.exe" "%USERPROFILE%\Desktop\MacRevival Simple Help.exe"
+ECHO. The Simple Help installation is complete.
 
 GOTO:EOF
