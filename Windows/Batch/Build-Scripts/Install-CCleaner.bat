8c5843a8ce9fdc3a111404ea8da55e654532b652
Justin Muniz <justin.muniz@maine.edu>
2016-09-05 21:21:40 -0400

    Update Install-CCleaner.bat
    Configured the CCleaner installer to run without a window.

--- Windows/Batch/Build-Scripts/Install-CCleaner.bat
+++ Windows/Batch/Build-Scripts/Install-CCleaner.bat
@@ -9,6 +9,6 @@ ECHO. The CCleaner Installer is opening in another window...
 %USERPROFILE%\Downloads\ccsetup520.exe
 ECHO. The CCleaner installation is complete.
 ECHO. Deleting temporary CCleaner installation files...
-DEL %USERPROFILE%\Downloads\ccsetup520.exe
+DEL %USERPROFILE%\Downloads\ccsetup520.exe /S
 
 GOTO:EOF
