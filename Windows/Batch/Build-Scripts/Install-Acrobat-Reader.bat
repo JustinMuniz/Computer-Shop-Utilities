53ed6ee4c11221f12f5ccc22d84f244d327f4a4f
Justin Muniz <justin.muniz@maine.edu>
2016-09-05 22:43:58 -0400

    Update Install-Acrobat-Reader.bat
    Added -install switch to the installation program to start installation without human interaction.
    Used START instead of CALL so that the "finish" screen won't hang up automated queue.

--- Windows/Batch/Build-Scripts/Install-Acrobat-Reader.bat
+++ Windows/Batch/Build-Scripts/Install-Acrobat-Reader.bat
@@ -6,7 +6,7 @@ ECHO.
 ECHO. Initializing Acrobat Reader download.
 bitsadmin.exe /transfer "Acrobat Reader Installer" https://admdownload.adobe.com/bin/live/readerdc_en_xa_install.exe %USERPROFILE%\Downloads\readerdc_en_xa_install.exe
 ECHO. The Acrobat Reader Installer is opening in another window...
-%USERPROFILE%\Downloads\readerdc_en_xa_install.exe
+START %USERPROFILE%\Downloads\readerdc_en_xa_install.exe -install
 ECHO. The Acrobat Reader installation is complete.
 ECHO. Deleting temporary Acrobat Reader installation files...
 DEL %USERPROFILE%\Downloads\readerdc_en_xa_install.exe
