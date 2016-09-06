77b28009d53303c41dcc3f4a4a753ec8152f20e9
Justin Muniz <justin.muniz@maine.edu>
2016-09-05 22:54:23 -0400

    Update Install-JRT.bat
    Moved JRT to desktop to keep for other scripts.

--- Windows/Batch/Build-Scripts/Install-JRT.bat
+++ Windows/Batch/Build-Scripts/Install-JRT.bat
@@ -4,11 +4,7 @@ SETLOCAL
 CLS
 ECHO.
 ECHO. Initializing Junkware Removal Tool download.
-bitsadmin.exe /transfer "Junkware Removal Tool Installer" http://data-cdn.mbamupdates.com/web/JRT.exe %USERPROFILE%\Downloads\JRT.exe
-ECHO. The Junkware Removal Tool Installer is opening in another window...
-%USERPROFILE%\Downloads\JRT.exe
+bitsadmin.exe /transfer "Junkware Removal Tool" http://data-cdn.mbamupdates.com/web/JRT.exe "%USERPROFILE%\Downloads\Junkware Removal Tool.exe"
 ECHO. The Junkware Removal Tool installation is complete.
-ECHO. Deleting temporary Junkware Removal Tool installation files...
-DEL %USERPROFILE%\Downloads\JRT.exe
 
 GOTO:EOF
