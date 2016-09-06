a9e8d1c516000805a6dc2781f1d92444789658c1
Justin Muniz <justin.muniz@maine.edu>
2016-09-05 21:48:48 -0400

    Update Install-Simple-Help.bat
    Changed bits priority to foreground as the remote server cannot support background downloads.
    Tested as fully working.

--- Windows/Batch/Build-Scripts/Install-Simple-Help.bat
+++ Windows/Batch/Build-Scripts/Install-Simple-Help.bat
@@ -4,7 +4,7 @@ SETLOCAL
 CLS
 ECHO.
 ECHO. Initializing Simple Help download.
-bitsadmin.exe /transfer "MacRevival Simple Help" "http://macrevival.noip.me/customer/Remote%20Support-windows32-offline.exe?language=en&hostname=http%3A%2F%2Fmacrevival.noip.me&ie=ie.exe" "%USERPROFILE%\Desktop\MacRevival Simple Help.exe"
+bitsadmin.exe /transfer "MacRevival Simple Help" /priority FOREGROUND "http://macrevival.noip.me/customer/Remote%20Support-windows32-offline.exe?language=en&hostname=http%3A%2F%2Fmacrevival.noip.me&ie=ie.exe" "%USERPROFILE%\Desktop\MacRevival Simple Help.exe"
 ECHO. The Simple Help installation is complete.
 
 GOTO:EOF
