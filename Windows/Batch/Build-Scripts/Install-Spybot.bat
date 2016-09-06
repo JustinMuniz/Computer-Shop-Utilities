298ee3ee47f57832a8ba9b9a119fd7edaaf46e1b
Justin Muniz <justin.muniz@maine.edu>
2016-09-05 21:03:09 -0400

    Update Install-Spybot.bat
    Simplified installer download file name.
    Changed from normal installer package to Ninite installer package.

--- Windows/Batch/Build-Scripts/Install-Spybot.bat
+++ Windows/Batch/Build-Scripts/Install-Spybot.bat
@@ -4,11 +4,11 @@ SETLOCAL
 CLS
 ECHO.
 ECHO. Initializing Spybot Search and Destroy download.
-bitsadmin.exe /transfer "Spybot Search and Destroy Installer" http://updates3.safer-networking.org/spybot1/spybot-2.4.exe %USERPROFILE%\Downloads\spybot-2.4.exe
+bitsadmin.exe /transfer "Spybot Search and Destroy Installer" https://ninite.com/spybot2/ninite.exe %USERPROFILE%\Downloads\spybot.exe
 ECHO. The Spybot Search and Destroy Installer is opening in another window...
-%USERPROFILE%\Downloads\spybot-2.4.exe
+%USERPROFILE%\Downloads\spybot.exe
 ECHO. The Spybot Search and Destroy installation is complete.
 ECHO. Deleting temporary Spybot Search and Destroy installation files...
-DEL %USERPROFILE%\Downloads\spybot-2.4.exe
+DEL %USERPROFILE%\Downloads\spybot.exe
 
 GOTO:EOF
