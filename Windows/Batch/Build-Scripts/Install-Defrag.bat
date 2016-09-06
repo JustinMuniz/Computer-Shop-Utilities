ff1cd9777cd647b9e1814f4dea3c98192213e910
Justin Muniz <justin.muniz@maine.edu>
2016-09-05 21:20:06 -0400

    Update Install-Defrag.bat
    Configured the Ninite installer to run without a window.

--- Windows/Batch/Build-Scripts/Install-Defrag.bat
+++ Windows/Batch/Build-Scripts/Install-Defrag.bat
@@ -9,6 +9,6 @@ ECHO. The Auslogics Disk Defrag Installer is opening in another window...
 %USERPROFILE%\Downloads\disk-defrag-setup.exe
 ECHO. The Auslogics Disk Defrag installation is complete.
 ECHO. Deleting temporary Auslogics Disk Defrag installation files...
-DEL %USERPROFILE%\Downloads\disk-defrag-setup.exe
+DEL %USERPROFILE%\Downloads\disk-defrag-setup.exe /silent .
 
 GOTO:EOF
