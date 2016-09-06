771db37c9dd8a7808720dc30db75ee9c3c1f9b0c
Justin Muniz <justin.muniz@maine.edu>
2016-09-05 22:44:20 -0400

    Update Install-Flash-Player.bat
    Added -install switch to the installation program to start installation without human interaction.
    Used START instead of CALL so that the "finish" screen won't hang up automated queue.

--- Windows/Batch/Build-Scripts/Install-Flash-Player.bat
+++ Windows/Batch/Build-Scripts/Install-Flash-Player.bat
@@ -6,7 +6,7 @@ ECHO.
 ECHO. Initializing Flash Player download.
 bitsadmin.exe /transfer "Flash Player Installer" https://admdownload.adobe.com/bin/live/flashplayer22_xa_install.exe %USERPROFILE%\Downloads\flashplayer22_xa_install.exe
 ECHO. The Flash Player Installer is opening in another window...
-%USERPROFILE%\Downloads\flashplayer22_xa_install.exe
+START %USERPROFILE%\Downloads\flashplayer22_xa_install.exe -install
 ECHO. The Flash Player installation is complete.
 ECHO. Deleting temporary Flash Player installation files...
 DEL %USERPROFILE%\Downloads\flashplayer22_xa_install.exe
