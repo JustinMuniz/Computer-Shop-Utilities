56858d5b30809865319425254118b406f1c18dec
Justin Muniz <justin.muniz@maine.edu>
2017-03-18 20:40:56 -0400

    Update Batch-Menu.bat
    Updated copyright statement to account for a new year of development
    Corrected filename typos for new scripts
    Added experimental label to the system maintenance option, as it isn't currently implemented
    Changed current directory so that the build menu script can be run from the batch menu or as a stand-alone script

--- Windows/Batch/Batch-Menu.bat
+++ Windows/Batch/Batch-Menu.bat
@@ -1,11 +1,13 @@
 @ECHO OFF
 SETLOCAL
 
+cd Build-Scripts
+
 :menuLOOP
 	cls
 	echo.
 	echo.================    Windows System Helper Scripts    ================
-	ECHO.================        (c) 2016 Justin Muniz        ================
+	ECHO.================     (c) 2016 - 2017 Justin Muniz    ================
 	echo.
 	echo.
 	for /f "tokens=1,2,* delims=_ " %%A in ('"findstr /b /c:":menu_" "%~f0""') do (
@@ -16,13 +18,13 @@ SETLOCAL
 GOTO:menuLOOP
 
 :menu_1		Installation / Configuration
-	start Build-Scripts\Build-Menu.bat
+	start Build-Menu.bat
 GOTO:EOF
 
-:menu_2		System Maintenance
-	start RunMaintenance.bat
+:menu_2		System Maintenance (experimental)
+	start ..\Run-Maintenance.bat
 GOTO:EOF
 
 :menu_3		Favorite Shortcuts
-	start WindowsShortcuts.bat
+	start ..\Windows-Shortcuts.bat
 GOTO:EOF
